e9d07ccf17bef7e942c9394971767acaf88dcb8c
mark <markhinojosa1212@gmail.com>
2021-07-17 19:54:10 -0500

    added resources

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    console.log(`[TEST]!`);
+
+    res.send("Hello from Homepage.");
+});
+
+app.listen(PORT, () =>
+    console.log(`Server running on port : http://localhost:${PORT}`)
+);
